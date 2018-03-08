7287d7916be5dfb457b0d0a9eeec8c7b4893d328
Jordan Kay <jordanekay@mac.com>
2018-03-08 15:55:42 -0800

    Initial Commit

---
+++ Stagehand/StatusBar.swift
@@ -0,0 +1,40 @@
+//
+//  StatusBar.swift
+//  Stagehand
+//
+//  Created by Jordan Kay on 3/8/18.
+//  Copyright © 2018 Cultivr. All rights reserved.
+//
+
+open class StatusBarAnimatingViewController: UIViewController {
+    open var statusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+    public var currentStatusBarStyle: UIStatusBarStyle? {
+        didSet {
+            setNeedsStatusBarAppearanceUpdate()
+        }
+    }
+    
+    // MARK: UIViewController
+    final override public var preferredStatusBarStyle: UIStatusBarStyle {
+        return currentStatusBarStyle ?? statusBarStyle
+    }
+    
+    override open func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        guard let coordinator = transitionCoordinator, let viewController = navigationController?.topViewController as? StatusBarAnimatingViewController, !coordinator.isCancelled else { return }
+        viewController.currentStatusBarStyle = preferredStatusBarStyle
+        coordinator.animate {
+            viewController.currentStatusBarStyle = viewController.statusBarStyle
+        }
+    }
+}
+
+extension UINavigationController {
+    // MARK: UIViewController
+    open override var childViewControllerForStatusBarStyle: UIViewController? {
+        return topViewController
+    }
+}
---
+++ Stagehand/TransitionCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  TransitionCoordinator.swift
+//  Stagehand
+//
+//  Created by Jordan Kay on 3/8/18.
+//  Copyright © 2018 Cultivr. All rights reserved.
+//
+
+extension UIViewControllerTransitionCoordinator {
+    func animate(animations: @escaping () -> Void) {
+        if isInteractive {
+            animate(alongsideTransition: { _ in
+                animations()
+            }, completion: nil)
+        } else {
+            let curve = UInt(completionCurve.rawValue)
+            let options = UIViewAnimationOptions(rawValue: curve << 16)
+            UIView.animate(withDuration: transitionDuration, delay: 0, options: options, animations: animations, completion: nil)
+        }
+    }
+}
